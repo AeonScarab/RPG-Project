40a1f791f0c8517d7e031831bbacbf3dcaee8ae2
WHITEMYTH\Casey Baron <aeon.scarab@gmail.com>
2018-07-08 18:18:37 -1000

    Completed Unity E02 Interaction
    https://www.youtube.com/watch?v=9tePzyL6dgc&index=3&list=PLPV2KyIb3jR4KLGCCAciWQ5qHudKtYeP7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
 
+    public Interactable focus;
+
     public LayerMask movementMask;
 
     Camera cam;
@@ -18,29 +20,64 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // If we press left mouse
 		if(Input.GetMouseButtonDown(0))
         {
+            // We create a ray
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
+            // If the ray hits
             if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
-                motor.MoveToPoint(hit.point);
+                motor.MoveToPoint(hit.point);   // Move to where we hit
 
-                // Stop focusing any objects
+                RemoveFocus();
             }
         }
 
+        // If we press right mouse
         if (Input.GetMouseButtonDown(1))
         {
+            // We create a ray
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
+            // If the ray hits
             if (Physics.Raycast(ray, out hit, 100))
             {
-                // Check if we hit an interactible
-                // If we did set this as our focus
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
             }
         }
+    }
+
+    void SetFocus (Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+
+        focus = newFocus;
+        newFocus.OnFocused(transform);
+        motor.FollowTarget(newFocus);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus.OnDefocused();
+        focus = null;
+        motor.StopFollowingTarget();
     }
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,15 +6,53 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMotor : MonoBehaviour {
 
-    NavMeshAgent agent;
+    Transform target;       // Target to follow
+    NavMeshAgent agent;     // Reference to our agent
 
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
 	}
 	
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
+
 	public void MoveToPoint (Vector3 point)
     {
         agent.SetDestination(point);
+    }
+
+    public void FollowTarget (Interactable newTarget)
+    {
+        agent.stoppingDistance = newTarget.radius * 0.8f;
+        agent.updateRotation = false;
+
+        target = newTarget.interactionTransform;
+    }
+
+    public void StopFollowingTarget()
+    {
+        agent.stoppingDistance = 0f;
+        agent.updateRotation = true;
+
+        target = null;
+    }
+
+    void FaceTarget()
+    {
+        // Get a direction from our target
+        Vector3 direction = (target.position - transform.position).normalized;
+
+        // Rotate ourselves to look in that direction (except in y)
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+
+        // Smooth out that rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 }
