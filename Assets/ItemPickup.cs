2f7508cb4f120146c5c9a4f2a12d8c242383f95a
WHITEMYTH\Casey Baron <aeon.scarab@gmail.com>
2018-08-12 15:17:55 -1000

    Completed Unity E04 Items
    https://www.youtube.com/watch?v=HQNl3Ff2Lpo&list=PLPV2KyIb3jR4KLGCCAciWQ5qHudKtYeP7&index=5

---
+++ Assets/ItemPickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : Interactable {
+
+    public Item item;
+
+    // Override method and run everything in Interact() method.
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    void PickUp ()
+    {
+        Debug.Log("Picking up " + item.name);
+        // Add to inventory
+        bool wasPickedUp = Inventory.instance.Add(item);
+        if (wasPickedUp)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
